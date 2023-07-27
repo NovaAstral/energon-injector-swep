d8f1f8411fea6c051607162de5cf425a69e01921
NovaAstral <mariofluddd@gmail.com>
2023-07-26 23:02:21 -0300

    Fixed and updated the Energon Healer - Fixed several parts of the Energon Healer - Made it heal the target incrementally (meaning it cycles from eg: 20, 21, 22, etc up to 40 instead of instantly going from 20 to 40) - Made it properly have 10 uses before you need to recharge it (recharge not implemented, it uses ammo that cannot be refilled through normal means)

--- lua/weapons/weapon_energon.lua
+++ lua/weapons/weapon_energon.lua
@@ -1,26 +1,24 @@
-
-AddCSLuaFile()
-
 SWEP.PrintName = "Energon"
-SWEP.Author = "Spok"
-SWEP.Purpose = "RMB - heal yourself, LMB - heal someone else."
+SWEP.Author = "Nova Astral"
+SWEP.Purpose = "LMB - Heal Target NPC or Player, RMB - Heal Yourself"
 
 SWEP.Slot = 5
 SWEP.SlotPos = 3
-SWEP.DrawAmmo = true	
 SWEP.Category = "Disposable Transformers"
 
 SWEP.Spawnable = true
 
-SWEP.ViewModel = Model( "models/megarexfoc/viewmodels/c_energon_injector_stim.mdl" )
-SWEP.WorldModel = Model( "models/megarexfoc/w_energon_injector.mdl" )
+SWEP.DrawAmmo = true
+
+SWEP.ViewModel = Model("models/megarexfoc/viewmodels/c_energon_injector_stim.mdl")
+SWEP.WorldModel = Model("models/megarexfoc/w_energon_injector.mdl")
 SWEP.ViewModelFOV = 75
 SWEP.UseHands = true
 
-SWEP.Primary.ClipSize = 10
-SWEP.Primary.DefaultClip = 1
+SWEP.Primary.ClipSize = -1
+SWEP.Primary.DefaultClip = 10
 SWEP.Primary.Automatic = false
-SWEP.Primary.Ammo = "none"
+SWEP.Primary.Ammo = "HelicopterGun"
 
 SWEP.Secondary.ClipSize = -1
 SWEP.Secondary.DefaultClip = -1
@@ -29,100 +27,85 @@ SWEP.Secondary.Ammo = "none"
 
 SWEP.HealAmount = 20 -- Maximum heal amount per use
 SWEP.MaxAmmo = 10 -- Maxumum ammo
+SWEP.HealDist = 45 -- Distance you can heal other players/npcs from
 
 local HealSound = Sound( "cybertronian/energon_inject.wav" )
 local DenySound = Sound( "WallHealth.Deny" )
 
+if SERVER then
+	AddCSLuaFile()
+end
+
 function SWEP:Initialize()
+	self:SetHoldType("slam")
 
-	self:SetHoldType( "slam" )
-	
+	if(CLIENT) then return end
+end
 
-	if ( CLIENT ) then return end
+function HealTarget(ent,owner)
+	local self = owner:GetWeapon("weapon_energon")
+
+	if(IsValid(ent) and ent:IsPlayer() or ent:IsNPC()) then
+		print("valid")
+		if(self:Ammo1() > 0 and ent:Health() < ent:GetMaxHealth()) then
+			timer.Create("EnergonHeal" .. self:EntIndex(),0.1,self.HealAmount,function()
+				if(IsValid(ent)) then
+					ent:SetHealth(math.Clamp(ent:Health() + 1,0,ent:GetMaxHealth()))
+				else
+					timer.Stop("EnergonHeal" .. self:EntIndex())
+				end
+			end)
+			
+			self:EmitSound(HealSound)
+
+			if(ent == owner) then
+				self:SendWeaponAnim(ACT_VM_SECONDARYATTACK)
+			else
+				self:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
+			end
+
+			self:TakePrimaryAmmo(1)
+			self:SetNextSecondaryFire(CurTime() + self:SequenceDuration() + 1)
+
+			timer.Create("weapon_idle" .. self:EntIndex(),self:SequenceDuration(),1,function()
+				if(IsValid(self)) then 
+					self:SendWeaponAnim(ACT_VM_IDLE)
+				end 
+			end)
+		else
+			self:EmitSound(DenySound)
+		end
+	end
 end
 
 function SWEP:PrimaryAttack()
+	local tr = self:GetOwner():GetEyeTraceNoCursor()
 
-	if ( CLIENT ) then return end
-
-	local tr = util.TraceLine( {
-		start = self.Owner:GetShootPos(),
-		endpos = self.Owner:GetShootPos() + self.Owner:GetAimVector() * 64,
-		filter = self.Owner
-	} )
-
-	local ent = tr.Entity
-
-	local need = self.HealAmount
-	if ( IsValid( ent ) ) then need = math.min( ent:GetMaxHealth() - ent:Health(), self.HealAmount ) end
-
-	if ( IsValid( ent ) && self:Clip1() >= 1 && ( ent:IsPlayer() or ent:IsNPC() ) && ent:Health() < 100 ) then
-
-		self:TakePrimaryAmmo( 1 )
-		
-
-		ent:SetHealth( math.min( ent:GetMaxHealth(), ent:Health() + need ) )
-		ent:EmitSound( HealSound )
-
-		self:SendWeaponAnim( ACT_VM_PRIMARYATTACK )
-
-		self:SetNextPrimaryFire( CurTime() + self:SequenceDuration() + 0.5 )
-		self.Owner:SetAnimation( PLAYER_ATTACK1 )
-
-		-- Even though the viewmodel has looping IDLE anim at all times, we need this to make fire animation work in multiplayer
-		timer.Create( "weapon_idle" .. self:EntIndex(), self:SequenceDuration(), 1, function() if ( IsValid( self ) ) and self:Clip1() >= 1  then self:SendWeaponAnim( ACT_VM_IDLE ) else self:Remove () end end )
-
+	if(self:GetOwner():GetShootPos():Distance(tr.HitPos) <= self.HealDist and IsValid(tr.Entity)) then
+		HealTarget(tr.Entity,self:GetOwner())
 	else
-
-		self.Owner:EmitSound( DenySound )
-		self:SetNextPrimaryFire( CurTime() + 1 )
-
+		self:EmitSound(DenySound)
 	end
+
+	self:SetNextPrimaryFire(CurTime() + 1)
 end
 
 function SWEP:SecondaryAttack()
+	HealTarget(self:GetOwner(),self:GetOwner())
 
-	if ( CLIENT ) then return end
-
-	local ent = self.Owner
-
-	local need = self.HealAmount
-	if ( IsValid( ent ) ) then need = math.min( ent:GetMaxHealth() - ent:Health(), self.HealAmount ) end
-
-	if ( IsValid( ent ) && self:Clip1() >= 1 && ent:Health() < ent:GetMaxHealth() ) then
-
-		self:TakePrimaryAmmo( 1 )
-
-		ent:SetHealth( math.min( ent:GetMaxHealth(), ent:Health() + need ) )
-		ent:EmitSound( HealSound )
-
-		self:SendWeaponAnim( ACT_VM_SECONDARYATTACK )
-
-		self:SetNextSecondaryFire( CurTime() + self:SequenceDuration() + 1 )
-		self.Owner:SetAnimation( PLAYER_ATTACK1 )
-
-		timer.Create( "weapon_idle" .. self:EntIndex(), self:SequenceDuration(), 1, function() if ( IsValid( self ) ) and  self:Clip1() >= 1  then self:SendWeaponAnim( ACT_VM_IDLE ) else self:Remove () end end )
-
-	else
-
-		ent:EmitSound( DenySound )
-		self:SetNextSecondaryFire( CurTime() + 1 )
-
-	end
+	self:SetNextSecondaryFire(CurTime() + 1)
 end
 
 function SWEP:OnRemove()
-
-	timer.Stop( "weapon_idle" .. self:EntIndex() )
-
+	timer.Stop("weapon_idle" .. self:EntIndex())
+	timer.Stop("EnergonHeal" .. self:EntIndex())
 end
 
 function SWEP:Holster()
-
-	timer.Stop( "weapon_idle" .. self:EntIndex() )
+	timer.Stop("weapon_idle" .. self:EntIndex())
+	timer.Stop("EnergonHeal" .. self:EntIndex())
 
 	return true
-
 end
 
 
